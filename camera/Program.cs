3cc494009bb940dc97201d8dd0cab3d78f2cc1f5
noodles623 <dpn28@cornell.edu>
2022-03-12 17:59:35 -0500

    Simple upload from camera

--- camera/Program.cs
+++ camera/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Client.Samples
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            // Parse application parameters
+            Parameters parameters = new Parameters();
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+                .WithParsed(parsedParams =>
+                {
+                    parameters = parsedParams;
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
+
+            using var deviceClient = DeviceClient.CreateFromConnectionString(
+                parameters.PrimaryConnectionString,
+                parameters.TransportType);
+            
+            var sample = new FileUpload(deviceClient);
+            await sample.RunSampleAsync();
+
+            await deviceClient.CloseAsync();
+
+            Console.WriteLine("Done.");
+            return 0;
+        }
+
+    }
+}
