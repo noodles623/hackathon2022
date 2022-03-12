2dd159e28bfeda291a66ceaea571ac1f2ab84152
noodles623 <dpn28@cornell.edu>
2022-03-12 18:21:21 -0500

    backend image upload notification

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Azure.Devices;
+
+class Program
+{
+    static ServiceClient serviceClient;
+    static string connectionString = "HostName=camerahub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=Eyrt7QEb47BrfN2SrQPJ3Joid44KeDwan0LJiGI2vJo=";
+
+    [Obsolete]
+    public static void Main()
+    {
+        Console.WriteLine("Receive file upload notifications\n");
+        serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+        ReceiveFileUploadNotificationAsync();
+        Console.WriteLine("Press Enter to exit\n");
+        Console.ReadLine();
+    }
+
+    [Obsolete]
+    private async static void ReceiveFileUploadNotificationAsync()
+    {
+        var notificationReceiver = serviceClient.GetFileNotificationReceiver();
+        Console.WriteLine("\nReceiving file upload notification from service");
+        while (true)
+        {
+            var fileUploadNotification = await notificationReceiver.ReceiveAsync();
+            if (fileUploadNotification == null) continue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Received file upload notification: {0}",
+              string.Join(", ", fileUploadNotification.BlobName));
+            Console.ResetColor();
+            await notificationReceiver.CompleteAsync(fileUploadNotification);
+        }
+    }
+}
